e7380a85462a657d6af7adbed5ea42667760064e
Benedictus Jullian Pradana <benedictusjullian1@gmail.com>
2024-01-12 12:39:48 +0700

    insert feature for expense repo

--- config/raw_query.go
+++ config/raw_query.go
@@ -4,7 +4,7 @@ const (
 	SelectLastInsert = "SELECT balance FROM expenses ORDER BY created_at DESC LIMIT 1"
 	InsertExpense    = `INSERT INTO expenses 
     (date, amount, transaction_type, balance, descritption,created_at,updated_at)
-    VALUES ($1, $2, $3, $4, $5, $6, $7)
+    VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id
   `
 	SelectExpenseBetwenDate = `
     SELECT * FROM expenses 
--- repository/expense_repository.go
+++ repository/expense_repository.go
@@ -2,7 +2,11 @@ package repository
 
 import (
 	"database/sql"
+	"expense-tracker/config"
 	"expense-tracker/model"
+	"fmt"
+	"log"
+	"time"
 )
 
 type ExpenseRepository interface {
@@ -20,7 +24,44 @@ func NewExpenseRepository(db *sql.DB) ExpenseRepository {
 }
 
 func (e *expenseRepository) CreateExpense(payload model.Expense) (model.Expense, error) {
+	var err error
 	var expense model.Expense
+	currTime := time.Now().Local()
+	expense.CreatedAt = currTime
+	expense.UpdatedAt = &currTime
+	expense.Date = fmt.Sprintf("%d-%d-%d", currTime.Year(), currTime.Month(), currTime.Day())
+
+	getLastExpense := config.SelectLastInsert
+	insertExpense := config.InsertExpense
+	err = e.db.QueryRow(getLastExpense).Scan(&expense.Balance)
+	// handle jika database kosong
+	if err != nil {
+		log.Fatal(err)
+		expense.Balance = expense.Amount
+		err := e.db.QueryRow(
+			insertExpense, expense.Date,
+			expense.Amount, expense.TransactionType,
+			expense.Balance, expense.Description,
+			expense.CreatedAt, expense.UpdatedAt).Scan(&expense.ID)
+		if err != nil {
+			return model.Expense{}, err
+		}
+	}
+
+	if expense.TransactionType == "CREDIT" {
+		expense.Balance = expense.Balance + expense.Amount
+	} else {
+		expense.Balance = expense.Balance - expense.Amount
+	}
+
+	err = e.db.QueryRow(
+		insertExpense, expense.Date,
+		expense.Amount, expense.TransactionType,
+		expense.Balance, expense.Description,
+		expense.CreatedAt, expense.UpdatedAt).Scan(&expense.ID)
+	if err != nil {
+		return model.Expense{}, err
+	}
 
 	return expense, nil
 }
