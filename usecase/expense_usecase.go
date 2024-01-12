c1f36dbdabd19db9a14e8ba9fec484cc252715f2
Benedictus Jullian Pradana <benedictusjullian1@gmail.com>
2024-01-12 14:13:59 +0700

    finish creating usecase

---
+++ usecase/expense_usecase.go
@@ -0,0 +1,60 @@
+package usecase
+
+import (
+	"expense-tracker/model"
+	"expense-tracker/repository"
+	sharedmodel "expense-tracker/shared/shared_model"
+)
+
+type ExpenseUsecase interface {
+	CreateExpense(payload model.Expense) (model.Expense, error)
+	GetExpense(startDate string, endDate string, page int, size int) ([]model.Expense, sharedmodel.Paging, error)
+	GetExpenseById(id string) (model.Expense, error)
+	GetExpenseByType(id string) ([]model.Expense, error)
+}
+
+type expenseUsecase struct {
+	expenseRepository repository.ExpenseRepository
+}
+
+func NewTaskUsecase(expenseRepository repository.ExpenseRepository) ExpenseUsecase {
+	return &expenseUsecase{
+		expenseRepository: expenseRepository,
+	}
+}
+
+func (e *expenseUsecase) CreateExpense(payload model.Expense) (model.Expense, error) {
+	expense, err := e.expenseRepository.CreateExpense(payload)
+	if err != nil {
+		return model.Expense{}, err
+	}
+
+	return expense, nil
+}
+
+func (e *expenseUsecase) GetExpense(startDate string, endDate string, page int, size int) ([]model.Expense, sharedmodel.Paging, error) {
+	expenses, paging, err := e.expenseRepository.GetExpense(startDate, endDate, page, size)
+	if err != nil {
+		return []model.Expense{}, sharedmodel.Paging{}, err
+	}
+
+	return expenses, paging, nil
+}
+
+func (e *expenseUsecase) GetExpenseById(id string) (model.Expense, error) {
+	expense, err := e.expenseRepository.GetExpenseById(id)
+	if err != nil {
+		return model.Expense{}, err
+	}
+
+	return expense, nil
+}
+
+func (e *expenseUsecase) GetExpenseByType(id string) ([]model.Expense, error) {
+	expenses, err := e.expenseRepository.GetExpenseByType(id)
+	if err != nil {
+		return nil, err
+	}
+
+	return expenses, nil
+}
