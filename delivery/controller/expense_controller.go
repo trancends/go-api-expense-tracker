b0bb035c9c56dea3dc63e350442fb24bcc9fe4fa
Benedictus Jullian Pradana <benedictusjullian1@gmail.com>
2024-01-12 15:31:37 +0700

    create route handler for get all task, get task by id , and get task by transaction type

--- delivery/controller/expense_controller.go
+++ delivery/controller/expense_controller.go
@@ -5,6 +5,8 @@ import (
 	"expense-tracker/shared/common"
 	"expense-tracker/usecase"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -45,4 +47,69 @@ func (e *ExpenseController) CreateHandler(ctx *gin.Context) {
 }
 
 func (e *ExpenseController) GetAllTask(ctx *gin.Context) {
+	pageParam := ctx.Param("page")
+	sizeParam := ctx.Param("size")
+	startDate := ctx.Param("startDate")
+	endDate := ctx.Param("endDate")
+
+	if pageParam == "" || sizeParam == "" || startDate == "" || endDate == "" {
+		common.SendErrorResponse(ctx, http.StatusBadRequest, "pagepageParam or sizeParam or startDate or endDate cant be empty")
+		return
+	}
+
+	_, err := time.Parse("2006-01-02", startDate)
+	if err != nil {
+		common.SendErrorResponse(ctx, http.StatusBadRequest, "invalid startDate")
+		return
+	}
+	_, err = time.Parse("2006-01-02", endDate)
+	if err != nil {
+		common.SendErrorResponse(ctx, http.StatusBadRequest, "invalid endDate")
+		return
+	}
+
+	page, err := strconv.Atoi(pageParam)
+	if err != nil {
+		common.SendErrorResponse(ctx, http.StatusBadRequest, "invalid page param")
+		return
+	}
+	size, err := strconv.Atoi(sizeParam)
+	if err != nil {
+		common.SendErrorResponse(ctx, http.StatusBadRequest, "invalid psize param")
+		return
+	}
+
+	expenses, paging, err := e.expenseUC.GetExpense(startDate, endDate, page, size)
+	if err != nil {
+		common.SendErrorResponse(ctx, http.StatusInternalServerError, "failed to get expenses "+err.Error())
+		return
+	}
+
+	common.SendPagedResponse(ctx, expenses, paging, "success")
+}
+
+func (a *ExpenseController) GetTaskById(ctx *gin.Context) {
+	id := ctx.Param("id")
+	expense, err := a.expenseUC.GetExpenseById(id)
+	if err != nil {
+		common.SendErrorResponse(ctx, http.StatusBadRequest, "invalid id or id doesnt exist "+err.Error())
+		return
+	}
+
+	common.SendSingleResponse(ctx, expense, "success")
+}
+
+func (a *ExpenseController) GetTaskByType(ctx *gin.Context) {
+	transType := ctx.Param("type")
+	if transType != string(model.CREDIT) || transType != string(model.DEBIT) {
+		common.SendErrorResponse(ctx, http.StatusBadRequest, "invalid type")
+		return
+	}
+	expenses, err := a.expenseUC.GetExpenseByType(transType)
+	if err != nil {
+		common.SendErrorResponse(ctx, http.StatusInternalServerError, "failed to fetch expenses")
+		return
+	}
+
+	common.SendSingleResponse(ctx, expenses, "success")
 }
